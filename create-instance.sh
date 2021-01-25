c8112db7ac2487c672b71448099acbf36576fbbb
Liam Fruzyna <liamfruzyna@gmail.com>
2021-01-25 14:57:51 -0600

    Added script to create new instances

---
+++ create-instance.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+name=$1
+pass=$2
+port=$3 # TODO intelligently generate port number
+
+sudo=[sudo password here]
+
+# TODO create volume of limited size, without overwriting contents of /config
+
+docker run -d --name code-server-${name} \
+		   -e PUID=1000 -e PGID=1000 -e TZ=America/Chicago \
+		   -e PASSWORD=$2 -e SUDO_PASSWORD=$sudo \
+		   -p $port:8443 \
+		   --cpus 0.5 -m 2.0g --memory-swap 5g \
+		   --restart unless-stopped \
+		   mail929/code-server-frc-java
+
+# TODO add reverse proxy entry
