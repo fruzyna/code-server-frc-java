cd3ffdfdd8c15edf3b3245a0d9e4941c2f86d678
Liam Fruzyna <liamfruzyna@gmail.com>
2021-01-26 10:47:59 -0600

    Added some safeguards

--- gui.py
+++ gui.py
@@ -21,42 +21,63 @@ class ServerHandler(http.server.SimpleHTTPRequestHandler):
             query = {}
             for q in queries:
                 parts = q.split('=')
-                query[parts[0]] = parts[1]
+                query[parts[0]] = parts[1].lower()
 
             # determine next port
-            process = subprocess.Popen(['docker', 'container', 'ls', '--format', '"{{.Ports}}"', '--filter', 'code-server-*'], stdout=subprocess.PIPE)
-            output, error = process.communicate()
-            port_str = str(output)
-            if ':' in port_str and '-' in port_str:
-                ports = [ int(port[port.index(':')+1:port.index('-')]) for port in port_str.splitlines() ]
+            port_str = str(subprocess.check_output(['docker', 'container', 'ls', '--format', '{{.Ports}}', '--filter', 'name=code-server-*']))[2:-1]
+            ports = [ int(port[port.index(':')+1:port.index('-')]) for port in port_str.split('\\n') if ':' in port and '-' in port ]
+            if len(ports) > 0:
                 port = max(ports) + 1
             else:
                 port = MIN_PORT
 
+            # determine taken names
+            name_str = str(subprocess.check_output(['docker', 'container', 'ls', '--format', '{{.Names}}', '--filter', 'name=code-server-*']))[2:-1]
+            names = [ name.split('-')[2] for name in name_str.split('\\n') if '-' in name ]
+
             # check for correct parameters
             if port >= MIN_PORT and port <= MAX_PORT:
-                if 'name' in query and 'pass' in query:
-                    if 'code' in query and query['code'] == PASSWORD:
-                        # launch
-                        subprocess.Popen(['./create-instance.sh', query['name'], query['pass'], str(port)])
-                        ip = socket.gethostbyname(socket.gethostname())
-                        url = 'http://{0}:{1}</a>'.format(ip, port)
-                        # use if using a reverse proxy
-                        #ip = 'code.mydomain.tld'
-                        #url = 'http://{0}/{1}</a>'.format(ip, query['name'])
+                if 'name' in query and query['name'].isalnum() and query['name'] not in names:
+                    if 'pass' in query and query['pass'].isalnum():
+                        if 'code' in query and query['code'] == PASSWORD:
+                            # launch
+                            subprocess.Popen(['./create-instance.sh', query['name'], query['pass'], str(port)])
+                            ip = socket.gethostbyname(socket.gethostname())
+                            url = 'http://{0}:{1}</a>'.format(ip, port)
+                            # use if using a reverse proxy
+                            #ip = 'code.mydomain.tld'
+                            #url = 'http://{0}/{1}</a>'.format(ip, query['name'])
 
-                        # send redirect page
+                            # send redirect page
+                            self.send_response(200)
+                            self.send_header('Content-type', 'text/html')
+                            self.end_headers()
+                            self.wfile.write(str.encode('<meta http-equiv="refresh" content="10; URL={0}" /><h1>Redirecting to code-server in 10 seconds</h1><a href="{0}">{0}</a>'.format(url)))
+                            return
+                        else:
+                            self.send_response(200)
+                            self.send_header('Content-type', 'text/html')
+                            self.end_headers()
+                            self.wfile.write(str.encode('<h1>Invalid access code</h1>'))
+                            return
+                    else:
                         self.send_response(200)
                         self.send_header('Content-type', 'text/html')
                         self.end_headers()
-                        self.wfile.write(str.encode('<meta http-equiv="refresh" content="10; URL={0}" /><h1>Redirecting to code-server in 10 seconds</h1><a href="{0}">{0}</a>'.format(url)))
+                        self.wfile.write(str.encode('<h1>Invalid password</h1>'))
                         return
-                    else:
-                        print('invalid password')
                 else:
-                    print('invalid user')
+                    self.send_response(200)
+                    self.send_header('Content-type', 'text/html')
+                    self.end_headers()
+                    self.wfile.write(str.encode('<h1>Invalid name</h1>'))
+                    return
             else:
-                print('invalid port')
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                self.wfile.write(str.encode('<h1>Error assigning port</h1>'))
+                return
 
         return http.server.SimpleHTTPRequestHandler.do_GET(self)
 
