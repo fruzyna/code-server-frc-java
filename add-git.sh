ae189f77121f19eece5296bd93b003363a39ef87
Liam Fruzyna <liamfruzyna@gmail.com>
2021-01-31 12:58:29 -0600

    Clarification tweaks

--- add-git.sh
+++ add-git.sh
@@ -3,16 +3,29 @@
 echo "Use this tool to setup coder with GitHub"
 echo "Press Ctrl + C at anytime to cancel"
 
+# add name to git
 read -p "Enter your name: " name
 git config --global user.name $name
 
 read -p "Enter your email: " email
-git config --global user.email $email
-ssh-keygen -t ed25519 -C $email -f /config/.ssh/id_ed25519 -P ""
 
+# remove existing key if it exists
+rm ~/.ssh/id_ed25519 ~/.ssh/id_ed25519.pub
+
+# add email to git
+git config --global user.email $email
+
+# create SSH key
+ssh-keygen -t ed25519 -C $email -f /config/.ssh/id_ed25519 -P ""
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519
 
-echo "Copy and paste the following text to https://github.com/settings/ssh/new"
-echo ""
-cat ~/.ssh/id_ed25519.pub
+# walk through adding it to Github
+code-server ~/.ssh/id_ed25519.pub
+echo "A new file (id_ed25519.pub) will open."
+echo "1. Copy the entire text of the file."
+echo "2. Open https://github.com/settings/ssh/new and sign in if necessary."
+echo "3. Put \"code server\" into the small \"Title\" text box."
+echo "4. Paste the copied text into the large \"Key\" text box."
+echo "5. Press \"Add SSH key\", then you are done."
+#cat ~/.ssh/id_ed25519.pub
--- get-code.sh
+++ get-code.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 read -p "What year's robot code would you like? " year
+echo "Fetching $year robot code."
+echo "Answer \"yes\" if it asks if you would like to continue connecting."
 git clone git@github.com:wildstang/${year}_robot_software
 code-server --reuse-window ${year}_robot_software
